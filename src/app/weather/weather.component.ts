618c0de5762e286afe3e8e088cd75149f78ce9da
suhailroushan13 <suhailroushan13@gmail.com>
2023-04-16 16:33:09 +0000

    Weather App

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css'],
+})
+export class WeatherComponent implements OnInit {
+  myWeather: any;
+  temp: number = 0;
+  feels: number = 0;
+  humidity: number = 0;
+  pressure: number = 0;
+  summary: string = '';
+  icon: string = '';
+  city: string = 'Hyderabad';
+  units: string = 'Sri Ram Nagar';
+  des: string = '';
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.weatherService.getWeather(this.city, this.units).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.myWeather = res;
+        console.log(this.myWeather);
+        this.temp = Math.floor(this.myWeather.main.temp - 273);
+        this.feels = Math.floor(
+          (this.myWeather.main.feels_like - 273.15) * 1.8 + 32
+        );
+        this.humidity = this.myWeather.main.humidity;
+        this.pressure = this.myWeather.main.pressure;
+        this.des = this.myWeather.weather[0].main;
+        this.summary = this.myWeather.weather[0].main;
+        this.icon =
+          `https://openweathermap.org/img/wn/` +
+          this.myWeather.weather[0].icon +
+          `@2x.png`;
+      },
+      error: (error) => {
+        console.log(error.message);
+      },
+
+      complete: () => {
+        console.log('API Called');
+      },
+    });
+  }
+}
